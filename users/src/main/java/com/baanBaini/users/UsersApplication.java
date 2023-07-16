206271532b593c5a06cdf31ae5eb015633b064d4
Danish Javed <danishjaved636@gmail.com>
2023-07-16 18:52:26 +0530

    Added Other MS and made all register to eureka server

--- users/src/main/java/com/baanBaini/users/UsersApplication.java
+++ users/src/main/java/com/baanBaini/users/UsersApplication.java
@@ -3,8 +3,10 @@ package com.baanBaini.users;
 import com.baanBaini.commons.utils.CheckAccess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersApplication {
 
 	public static void main(String[] args) {
